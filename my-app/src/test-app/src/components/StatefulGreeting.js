14a2f183a1fa279bb68907e847662ceab99dba8f
CHCheshire <pringl31@gmail.com>
2022-11-16 21:50:17 +0000

    Studying states

--- my-app/src/test-app/src/components/StatefulGreeting.js
+++ my-app/src/test-app/src/components/StatefulGreeting.js
@@ -7,22 +7,33 @@ class StatefulGreeting extends React.Component {
         this.state = {
             introduction: "Hello!",
             buttonText: "Exit",
+            count: 0
         };
     }
 
     handleClick() {
         this.setState({
-            introduction: "Goodbye!",
-        });
+            introduction: this.state.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+            buttonText: this.state.buttonText === "Enter" ? "Exit" : "Enter",
+        }, () =>
+        console.log('new state', this.state.introduction)
+        );
         console.log(this.state.introduction);
     }
 
-
+    Increment() {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+    
     render () {
         return (
             <div>
                 <h1>{this.state.introduction} {this.props.greeting}</h1>
                 <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+                <button onClick={() => this.Increment()}>Increment</button>
+                <p>You've clicked {this.state.count} times</p>
             </div>
         )
     }
